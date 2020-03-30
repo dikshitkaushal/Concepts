6740a16b3d14a0aca8a1652c4b1b90db7fbeb914
Dikshit Kaushal <47450978+dikshitkaushal@users.noreply.github.com>
2020-03-30 18:53:22 +0530

    movingobject_ by _ clicking

---
+++ raycasting_and_navmesh/Assets/scripts/player_movement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class player_movement : MonoBehaviour
+{
+    NavMeshAgent m_navmeshagent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_navmeshagent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Moveplayer(Vector3 hitpos)
+    {
+        m_navmeshagent.SetDestination(hitpos);
+    }
+}
